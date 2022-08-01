a50c41dc9ff9e572dda2e1954d5451b476f8610d
Jaroslav <garasym@gmail.com>
2022-08-01 12:55:29 +0300

    Read data from Excel

---
+++ src/main/java/com/softserve/tools/IUser.java
@@ -0,0 +1,16 @@
+package com.softserve.tools;
+
+public interface IUser {
+
+    String getName();
+
+    String getLogin();
+
+    String getPassword();
+
+    String getRole();
+
+    String getEmail();
+
+    String getAddress();
+}
