c6c442af751b5c759f737cc23077c883b2ef4121
Jaroslav <garasym@gmail.com>
2022-08-02 15:40:30 +0300

    unicode

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -8,6 +8,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //String unicode = "\uD83C\uDDFA\uD83C\uDDE6\uD83E\uDDD0";
+        String unicode ="\u041F\u0440\u0456\u0437\u0432\u0438\u0449\u0435, \u0456\u043C'\u044F, \u043F\u043E \u0431\u0430\u0442\u044C\u043A\u043E\u0432\u0456 \u043E\u0442\u0440\u0438\u043C\u0443\u0432\u0430\u0447\u0430";
+        System.out.println("unicode: " + unicode);
         //
         Scanner sc = new Scanner(System.in);
         System.out.print("a = ");
