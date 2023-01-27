b8a77d887b41a99a9a1d579ffaf076da99b94f7e
Jaroslav <garasym@gmail.com>
2023-01-27 10:58:17 +0200

    Jdbc. Example

---
+++ src/main/java/com/edu/App.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+// mvn archetype:generate -DgroupId=com.edu -DartifactId=javamvn -Dversion=1.0 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.0  -DinteractiveMode=false
