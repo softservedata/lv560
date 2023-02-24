f6234bfd8c46290fc4a81fb1d4d57c616b9f7ba0
Yaroslav <garasym@gmail.com>
2023-02-24 15:56:22 +0200

    Factory Method

---
+++ src/test/java/com/softserve/edu/opencart/pages/RegisterPage.java
@@ -0,0 +1,23 @@
+package com.softserve.edu.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPage extends AccountSidebarGuestPart {
+
+    //public RegisterPage(WebDriver driver) {
+    public RegisterPage() {
+        //super(driver);
+        initElements();
+    }
+
+    private void initElements() {
+        // init elements
+    }
+
+    // Page Object
+
+    // Functional
+
+    // Business Logic
+
+}
