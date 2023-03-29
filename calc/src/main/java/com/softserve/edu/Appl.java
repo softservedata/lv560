b05f7b910576f75c8585d4dce7355a86ba05357f
Yaroslav <garasym@gmail.com>
2023-03-29 11:54:41 +0300

    Multi-Modules

---
+++ calc/src/main/java/com/softserve/edu/Appl.java
@@ -0,0 +1,10 @@
+package com.softserve.edu;
+
+public final class Appl {
+
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        System.out.println("OLD 1 + 2 + ... = " + calc.add(1, 2));
+        System.out.println("done");
+    }
+}
